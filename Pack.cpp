7048b9a72f548b1fa52e8b9e4105617289229ce2
rutvikparikh1 <115261602+rutvikparikh1@users.noreply.github.com>
2022-10-20 01:17:32 -0400

    Finished Autograder

--- Pack.cpp
+++ Pack.cpp
@@ -5,12 +5,6 @@
 
 using namespace std;
   
-  // EFFECTS: Initializes the Pack to be in the following standard order:
-  //          the cards of the lowest suit arranged from lowest rank to
-  //          highest rank, followed by the cards of the next lowest suit
-  //          in order from lowest to highest rank, and so on.
-  // NOTE: The standard order is the same as that in pack.in.
-  // NOTE: Do NOT use pack.in in your implementation of this function
   Pack::Pack(){
     cards[0] = Card(Card::RANK_NINE, Card::SUIT_SPADES);
     cards[1] = Card(Card::RANK_TEN, Card::SUIT_SPADES);
@@ -40,10 +34,6 @@ using namespace std;
     next = 0;
   }
 
-  // REQUIRES: pack_input contains a representation of a Pack in the
-  //           format required by the project specification
-  // MODIFIES: pack_input
-  // EFFECTS: Initializes Pack by reading from pack_input.
   Pack::Pack(std::istream& pack_input){
     for(int i = 0; i < PACK_SIZE; i++) {
       std::string rankI;
@@ -58,21 +48,15 @@ using namespace std;
     next = 0;
   }
 
-  // REQUIRES: cards remain in the Pack
-  // EFFECTS: Returns the next card in the pack and increments the next index
   Card Pack::deal_one(){
     next +=1;
     return cards[next-1];
   }
 
-  // EFFECTS: Resets next index to first card in the Pack
   void Pack::reset(){
     next = 0;
   }
 
-  // EFFECTS: Shuffles the Pack and resets the next index. This
-  //          performs an in shuffle seven times. See
-  //          https://en.wikipedia.org/wiki/In_shuffle.
   void Pack::shuffle(){
     for(int i=0; i<7; i++){ //iterates 7 times
       std::array<Card, PACK_SIZE/2> cards1;
@@ -88,14 +72,13 @@ using namespace std;
       for(int k=0; k<PACK_SIZE-1; k+=2){
         cards[k] = cards2[counter];
         cards[k+1] = cards1[counter];
-        cout << cards[k] << endl;
-        cout << cards[k+1] << endl;
+        //cout << cards[k] << endl;
+        //cout << cards[k+1] << endl;
         counter ++;
       }
     }
   }
 
-  // EFFECTS: returns true if there are no more cards left in the pack
   bool Pack::empty() const{
     return PACK_SIZE - next == 0;
   }
